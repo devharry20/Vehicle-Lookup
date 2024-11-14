17bbd5c64dcb6231e4dac21cf9b74d531f3b726c
Harry <Athonal2019@gmail.com>
2024-11-14 23:55:52 +0000

    modified class structure to allow for more data from seperate api

--- models.py
+++ models.py
@@ -22,6 +22,7 @@ class MotTest:
 
 @dataclass
 class Vehicle:
+    # MOT API data
     registration: str
     make: str
     model: str
@@ -33,4 +34,21 @@ class Vehicle:
     engineSize: Optional[str] = None
     motTestDueDate: Optional[str] = None
     hasOutstandingRecall: Optional[str] = None
-    motTests: List[MotTest] = field(default_factory=list) 
+    motTests: List[MotTest] = field(default_factory=list)
+
+    # VES API data
+    registrationNumber: Optional[str] = None
+    taxStatus: Optional[str] = None
+    taxDueDate: Optional[str] = None
+    motStatus: Optional[str] = None
+    yearOfManufacture: Optional[str] = None
+    engineCapacity: Optional[str] = None
+    co2Emissions: Optional[int] = None
+    markedForExport: Optional[str] = None
+    colour: Optional[str] = None
+    typeApproval: Optional[str] = None
+    dateOfLastV5CIssued: Optional[str] = None
+    motExpiryDate: Optional[str] = None
+    wheelplan: Optional[str] = None
+    monthOfFirstRegistration: Optional[str] = None
+    monthOfFirstDvlaRegistration: Optional[str] = None
